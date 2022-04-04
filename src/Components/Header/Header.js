46678027a0bee0b4389b69d0c7c5a188d66063c6
qurrotaainida <qurrotaainida250701@gmail.com>
2022-04-04 15:55:40 +0700

    header and search bar

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import style from './header.module.css'
+
+const Header = () => {
+    return (
+       <img src = "/images/logo.png" alt="Star Wars" className={style.logo}/>
+    );
+};
+
+export default Header;
